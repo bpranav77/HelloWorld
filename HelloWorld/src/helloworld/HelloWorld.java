2074d4ef5202f8fbace9a26619be80e978f12146
bp <bp@192.168.178.23>
2015-02-22 00:49:21 +0100

    second checkin

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is my first hello to the GitHub");
+System.out.println("This is my second hello to the GitHub");
 	}
 
 }
